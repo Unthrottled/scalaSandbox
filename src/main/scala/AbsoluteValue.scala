9167840ff7fecedb0e6cd673594b89ec55906359
DATBOI <alex@cyclicreference.space>
2018-03-24 20:27:35 -0500

    Look at me go

---
+++ src/main/scala/AbsoluteValue.scala
@@ -0,0 +1,18 @@
+object AbsoluteValue extends App {
+
+  def absoluteValue(n: Int): Int =
+    if (n < 0)
+      -n
+    else
+      n
+
+  private def formatAbsoluteValue(x: Int): String ={
+    val message = "The absolute value of %d is %d"
+    message.format(x, absoluteValue(x))
+  }
+
+
+  override def main(args: Array[String]): Unit =
+    println(formatAbsoluteValue(-42))
+
+}
