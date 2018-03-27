fbeb9ac10be48858d040344ae01bb80b46f16268
DATBOI <alex@cyclicreference.space>
2018-03-26 19:26:49 -0500

    Who says old dogs cannot learn new tricks?

--- src/main/scala/Currying.scala
+++ src/main/scala/Currying.scala
@@ -1,3 +1,11 @@
 object Currying extends App {
-  def curry[A,B,C] (f: (A, B)=> C): A => (B => C) 
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
+
+  override def main(args: Array[String]): Unit = {
+    def longToFunction = curry((a: Long, b: Int)=>a<b)
+    def intToBoolean = longToFunction(69L)
+    def bool = intToBoolean(420)
+    println(bool)
+  }
 }
