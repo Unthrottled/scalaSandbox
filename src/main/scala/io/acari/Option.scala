87e3c53c7719bb237368fba951bfd0626ec7144a
DATBOI <alex@cyclicreference.space>
2018-04-01 11:56:47 -0500

    Fixes the white space and added some of the functions in trait

--- src/main/scala/io/acari/Option.scala
+++ src/main/scala/io/acari/Option.scala
@@ -1,6 +1,33 @@
 package io.acari
 
-sealed trait Option[+T]
-case class Some[+T](get: T) extends Option[T]
-case object None extends Option[Nothing]
+sealed trait Option[+T]{
+  def map[U](f: T => U): Option[U]
+  def flatMap[U](f: T => Option[U]): Option[U]
+  def getOrElse[U >: T](default: => U): U
+  def orElse[U >: T](ob: => Option[U]): Option[U]
+  def filter(f: T => Boolean): Option[T]
+}
+case class Some[+T](get: T) extends Option[T] {
+  override def map[U](f: T => U): Option[U] = ???
+
+  override def flatMap[U](f: T => Option[U]): Option[U] = ???
+
+  override def getOrElse[U >: T](default: => U): U = ???
+
+  override def orElse[U >: T](ob: => Option[U]): Option[U] = ???
+
+  override def filter(f: T => Boolean): Option[T] = ???
+}
+case object None extends Option[Nothing] {
+  override def map[U](f: Nothing => U): Option[U] = ???
+
+  override def flatMap[U](f: Nothing => Option[U]): Option[U] = ???
+
+  override def getOrElse[U >: Nothing](default: => U): U = ???
+
+  override def orElse[U >: Nothing](ob: => Option[U]): Option[U] = ???
+
+  override def filter(f: Nothing => Boolean): Option[Nothing] = ???
+}
+
 
