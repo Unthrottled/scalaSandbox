9273756e86d12f0ea328aee5b8430561dd5ca5e7
DATBOI <alex@cyclicreference.space>
2018-03-31 19:46:20 -0500

    size of the trees

--- src/main/scala/io/acari/Tree.scala
+++ src/main/scala/io/acari/Tree.scala
@@ -4,3 +4,14 @@ sealed trait Tree[+T]
 case class Leaf[T](value: T) extends Tree[T]
 case class Branch[T](left: Tree[T], right: Tree[T]) extends Tree[T]
 
+object Tree extends App {
+
+  def size[T](root: Tree[T]): Int = root match {
+    case Leaf(_) => 1
+    case Branch(left, right) => 1 + size(left) + size(right)
+  }
+
+  override def main(args: Array[String]): Unit = {
+
+  }
+}
