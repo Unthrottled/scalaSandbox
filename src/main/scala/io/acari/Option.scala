faf1880d50973738a32983ca492177953700c578
DATBOI <alex@cyclicreference.space>
2018-04-01 12:29:36 -0500

    That was hard

--- src/main/scala/io/acari/Option.scala
+++ src/main/scala/io/acari/Option.scala
@@ -1,14 +1,31 @@
 package io.acari
 
-sealed trait Option[+T]{
-  def map[U](f: T => U): Option[U] = ???
-  def flatMap[U](f: T => Option[U]): Option[U] = ???
-  def getOrElse[U >: T](default: => U): U = ???
-  def orElse[U >: T](ob: => Option[U]): Option[U] = ???
-  def filter(f: T => Boolean): Option[T] = ???
+sealed trait Option[+T] {
+  def map[U](f: T => U): Option[U] = this match {
+    case None => None
+    case (s: Some[T]) => Some(f(s.get))
+  }
+
+  def flatMap[U](f: T => Option[U]): Option[U] = {
+    map(f) getOrElse None
+  }
+
+  def getOrElse[U >: T](default: => U): U = this match {
+    case None => default
+    case Some(t) => t
+  }
+
+  def orElse[U >: T](ob: => Option[U]): Option[U] =
+    this map (Some(_)) getOrElse ob
+
+  def filter(f: T => Boolean): Option[T] =
+    flatMap(t => if (f(t)) Some(t) else None)
+
+
 }
 
 case class Some[+T](get: T) extends Option[T]
+
 case object None extends Option[Nothing]
 
 
