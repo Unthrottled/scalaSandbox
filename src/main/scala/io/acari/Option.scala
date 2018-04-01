f1b5d90ccb7bd766c12147cdfaeb998cdde427a8
DATBOI <alex@cyclicreference.space>
2018-04-01 12:03:05 -0500

    more of the compile worky

--- src/main/scala/io/acari/Option.scala
+++ src/main/scala/io/acari/Option.scala
@@ -1,13 +1,14 @@
 package io.acari
 
 sealed trait Option[+T]{
-  def map[U](f: T => U): Option[U]
-  def flatMap[U](f: T => Option[U]): Option[U]
-  def getOrElse[U >: T](default: => U): U
-  def orElse[U >: T](ob: => Option[U]): Option[U]
-  def filter(f: T => Boolean): Option[T]
+  def map[U](f: T => U): Option[U] = ???
+  def flatMap[U](f: T => Option[U]): Option[U] = ???
+  def getOrElse[U >: T](default: => U): U = ???
+  def orElse[U >: T](ob: => Option[U]): Option[U] = ???
+  def filter(f: T => Boolean): Option[T] = ???
 }
-//case class Some[+T](get: T) extends Option[T]
-//case object None extends Option[Nothing]
+
+case class Some[+T](get: T) extends Option[T]
+case object None extends Option[Nothing]
 
 
