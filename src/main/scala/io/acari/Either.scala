66ef21d3ead2f471d6c915b18bdc3a399faf520d
DATBOI <alex@cyclicreference.space>
2018-04-05 06:53:46 -0500

    I think I did it?

--- src/main/scala/io/acari/Either.scala
+++ src/main/scala/io/acari/Either.scala
@@ -4,11 +4,19 @@ package io.acari
   * Forged in the flames of battle by alex.
   */
 sealed trait Either[+E, +T] {
-  def map[U](f: T => U): Either[E, U] = ???
+  def map[U](f: T => U): Either[E, U] = this match {
+    case Left(e) => Left(e)
+    case Right(t) => Right(f(t))
+  }
 
-  def flatMap[EE >: E, U](f: T => Either[EE, U]): Either[EE, U] = ???
-
-  def orElse[EE >: E, U >: T](u: => Either[EE, U]): Either[EE, U] = ???
+  def flatMap[EE >: E, U](f: T => Either[EE, U]): Either[EE, U] = this match {
+    case Left(e) => Left(e)
+    case Right(t) => f(t)
+  }
+  def orElse[EE >: E, U >: T](u: => Either[EE, U]): Either[EE, U] = this match {
+    case Left(_) => u
+    case Right(_) => this
+  }
 
   def map2[EE >: E, U, V](uE: Either[EE, U])(f: (T, U) => V): Either[EE, V] =
     flatMap(t => uE.map(u=> f(t, u)))
