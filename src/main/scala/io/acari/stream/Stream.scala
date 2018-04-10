9e938efcb98718da13567f489cfa8a2fff5c5a03
DATBOI <alex@cyclicreference.space>
2018-04-10 06:41:18 -0500

    A & B < T & U

--- src/main/scala/io/acari/stream/Stream.scala
+++ src/main/scala/io/acari/stream/Stream.scala
@@ -6,61 +6,61 @@ package io.acari.stream
 
 case object Empty extends Stream[Nothing]
 
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+case class Cons[+T](h: () => T, t: () => Stream[T]) extends Stream[T]
 
 
-trait Stream[+A] {
+trait Stream[+T] {
 
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+  def foldRight[U](z: => U)(f: (T, => U) => U): U = // The arrow `=>` in front of the argument type `U` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean =
+  def exists(p: T => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
-  final def find(f: A => Boolean): Option[A] = this match {
+  final def find(f: T => Boolean): Option[T] = this match {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
 
-  def toList: io.acari.List[A] =
-    foldRight[io.acari.List[A]](io.acari.Nil())((t, u)=>io.acari.Cons(t, u))
+  def toList: io.acari.List[T] =
+    foldRight[io.acari.List[T]](io.acari.Nil())((t, u)=>io.acari.Cons(t, u))
 
-  def take(n: Int): Stream[A] = ???
+  def take(n: Int): Stream[T] = ???
 
-  def drop(n: Int): Stream[A] = ???
+  def drop(n: Int): Stream[T] = ???
 
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  def takeWhile(p: T => Boolean): Stream[T] = ???
 
-  def forAll(p: A => Boolean): Boolean = ???
+  def forAll(p: T => Boolean): Boolean = ???
 
-  def headOption: Option[A] = ???
+  def headOption: Option[T] = ???
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
-  def startsWith[B](s: Stream[B]): Boolean = ???
+  def startsWith[U](s: Stream[U]): Boolean = ???
 }
 
 object Stream {
   val ones: Stream[Int] = Stream.cons(1, ones)
 
-  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+  def cons[T](hd: => T, tl: => Stream[T]): Stream[T] = {
     lazy val head = hd
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
 
-  def empty[A]: Stream[A] = Empty
+  def empty[T]: Stream[T] = Empty
 
-  def apply[A](as: A*): Stream[A] =
+  def apply[T](as: T*): Stream[T] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   def from(n: Int): Stream[Int] = ???
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
+  def unfold[T, S](z: S)(f: S => Option[(T, S)]): Stream[T] = ???
 }
