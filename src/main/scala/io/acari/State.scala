aa424c622259bcf89db8786101e2b381381b752e
DATBOI <alex@cyclicreference.space>
2018-04-17 05:49:01 -0500

    This alias stuff is confusing

--- src/main/scala/io/acari/State.scala
+++ src/main/scala/io/acari/State.scala
@@ -93,7 +93,14 @@ object RNG {
         (io.acari.Cons(i._1, t._1), t._2))
   }
 
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] ={
+    rng =>{
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a,b), rng3)
+    }
+  }
+
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
