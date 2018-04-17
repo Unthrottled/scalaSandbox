e4af11dd71d8ef076b9730dbb42a11f18dde74b8
DATBOI <alex@cyclicreference.space>
2018-04-17 18:29:09 -0500

    POOOWWWWWERRRRRRRRRRRR!

--- src/main/scala/io/acari/State.scala
+++ src/main/scala/io/acari/State.scala
@@ -91,11 +91,7 @@ object RNG {
   }
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] ={
-    rng =>{
-      val (a, rng2) = ra(rng)
-      val (b, rng3) = rb(rng2)
-      (f(a,b), rng3)
-    }
+    flatMap(ra)(a=>map(rb)(b=>f(a,b)))
   }
 
 
